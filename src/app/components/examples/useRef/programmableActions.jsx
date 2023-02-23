cb24f280d8f037a04d613d493f828d6b5247e364
Igor <zhigalovzenit112233@gmail.com>
2023-02-23 13:51:48 +0300

    passed useRef

--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
 import CardWrapper from "../../common/Card";
+import Divider from "../../common/divider";
 import SmallTitle from "../../common/typografy/smallTitle";
 const ProgrammableActionsExample = () => {
+    const inputRef = useRef();
+
+    const handleClick = () => {
+        inputRef.current.focus();
+    };
+
+    const handleChangeWidth = () => {
+        inputRef.current.style.width = "50%";
+    };
+
     return (
         <CardWrapper>
             <SmallTitle className="card-title">
                 Программируемые действия и свойства
             </SmallTitle>
+            <Divider />
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                ref={inputRef}
+                type="emaik"
+                className="form-control"
+                id="email"
+            />
+            <button className="btn btn-primary mt-2" onClick={handleClick}>
+                Focus input
+            </button>
+            <br />
+            <button
+                className="btn btn-secondary mt-2"
+                onClick={handleChangeWidth}
+            >
+                Изменить ширину
+            </button>
         </CardWrapper>
     );
 };
